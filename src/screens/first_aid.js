18982920006138a5c7670d91c99bfd69a517ef49
ajiq <haziqruslan27@gmail.com>
2024-06-13 05:39:12 +0800

    bug fixed

--- src/screens/first_aid.js
+++ src/screens/first_aid.js
@@ -16,19 +16,19 @@ const FirstAid = () => {
                 <h2 className="firstaid-title">Bleeding</h2>
                 <img src={bleedingImg} alt="Bleeding" className="firstaid-image" />
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Apply direct pressure to the wound using a sterile gauze pad or clean cloth.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Elevate the injured area above the level of the heart if there is no fracture.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Cover the area with a pressure bandage. If bleeding does not stop, apply additional dressings.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     If necessary, apply pressure to the artery with your hand.
                 </p>
             </div>
@@ -36,23 +36,23 @@ const FirstAid = () => {
                 <h2 className="firstaid-title">Choking</h2>
                 <img src={chokingImg} alt="Choking" className="firstaid-image" />
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Stand behind the person. Wrap your arms around the waist.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Make a fist with one hand. Position it slightly above the person's navel.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Grab the fist with the other hand. Press hard into the abdomen with quick inward and upward thrust.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Perform 5 abdominal thrusts (Heimlich maneuver).
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     If the person becomes unconscious, perform CPR.
                 </p>
             </div>
@@ -61,39 +61,39 @@ const FirstAid = () => {
                 <img src={cprImg1} alt="CPR" className="firstaid-image" />
                 <img src={cprImg2} alt="CPR" className="firstaid-image" />
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     If face down, put the person on his or her back while supporting the head, neck and back.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Place the heel of one hand over the person's breastbone. Place the other hand on top of the first hand. Keep your elbows straight.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Using your upper body push straight down compressing the chest to about 2 inches (5 cm). Push hard at a rate of 100 compression per minute.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     If trained for CPR, after 30 compressions, open the person's airway by placing your palm on the person's forehead and gently tilt the head back. With the other hand, gently lift the chin forward to open the airway.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Check for normal breathing, chest motion and listen for normal breath sounds.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Pinch the nostrils and cover the person's mouth with yours.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Give the first rescue breath and watch to see if the chest rises. If it does rise give the second breath. If the chest does not rise, repeat the head tilt, chin tilt and give the second breath.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Resume chest compressions.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Continue CPR until there are signs of movement or emergency personnel take over.
                 </p>
             </div>
@@ -101,15 +101,15 @@ const FirstAid = () => {
                 <h2 className="firstaid-title">Eye Injury</h2>
                 <img src={eyeInjuryImg} alt="Eye Injury" className="firstaid-image" />
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Don't rub the eye.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     For a foreign particle such as dirt, sand, or sliver of wood of metal, have the person pull the upper lid down, blink repeatedly and flush the eye with water.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     For any chemicals in the eye, immediately flush the eyes with lots of water.
                 </p>
             </div>
@@ -117,23 +117,23 @@ const FirstAid = () => {
                 <h2 className="firstaid-title">Fracture</h2>
                 <img src={fractureImg} alt="Fracture" className="firstaid-image" />
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Help support the injured area. Stop any bleeding by applying pressure with a sterile bandage or clean cloth.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Check for feeling, warmth and colour below fracture.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Immobilze the injured area. Apply a soft or hard splint above and below the fracture.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Apply ice or cold packs and elevate.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Treat for shock. Lay the person down and elevate legs.
                 </p>
             </div>
@@ -141,15 +141,15 @@ const FirstAid = () => {
                 <h2 className="firstaid-title">Shock</h2>
                 <img src={shockImg} alt="Shock" className="firstaid-image" />
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Lay the person down on his or her back.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     If it does not cause pain or injury, elevate the feet 12 inches (30 cm). Check for signs of breathing, coughing or movement. If absent, call emergency number and begin CPR.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Keep the person warm and comfortable. If choking, vomiting or bleeding from the mouth occur, turn the person on their side.
                 </p>
             </div>
@@ -157,19 +157,19 @@ const FirstAid = () => {
                 <h2 className="firstaid-title">Sprain</h2>
                 <img src={sprainImg} alt="Sprain" className="firstaid-image" />
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Rest the ankle or injured area.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Apply ice or cold packs wrapped in cloth to protect the skin.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Compress by lightly wrapping an elastic bandage around the injured area.
                 </p>
                 <p className="firstaid-description">
-                    <FaCheckCircle className='icon' />
+                    <FaCheckCircle className='checklist' />
                     Elevate the injured area above the level of the heart to reduce swelling.
                 </p>
             </div>
